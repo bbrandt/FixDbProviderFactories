f79f227432579309bd2a3844808b145d41b8ba52
Ben Brandt <bbrandt@users.noreply.github.com>
2017-10-13 09:59:09 -0500

    initial commit

---
+++ FixDbProviderFactories/FixDbProviderFactories.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using System.IO;
+
+namespace FixDbProviderFactories
+{
+    public class FixDbProviderFactories
+    {
+        public void Fix(string machineConfigFilePath)
+        {
+            XDocument xdoc = XDocument.Load(machineConfigFilePath);
+
+            XElement[] elements = xdoc.XPathSelectElements("//configuration/system.data/DbProviderFactories").ToArray();
+
+            if (elements.Any())
+            {
+                foreach (XElement anElement in elements)
+                {
+                    if (!anElement.HasElements)
+                        anElement.Remove();
+                }
+            }
+
+            // Let's not update the users machine.config just yet
+            var updatedMachineConfigFilePath = GetTemporaryFilePath(machineConfigFilePath);
+            xdoc.Save(machineConfigFilePath);
+        }
+
+        private string GetTemporaryFilePath(string filePath)
+        {
+            return Path.Combine(Path.GetDirectoryName(filePath), Path.GetRandomFileName());
+        }
+    }
+}
---
+++ FixDbProviderFactories/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace FixDbProviderFactories
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new FixDbProviderFactories().Fix(RuntimeEnvironment.SystemConfigurationFile);
+        }
+    }
+}
